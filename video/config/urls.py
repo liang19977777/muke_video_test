535858b533092b3669cbc84e1207590347b7ec7a
ldm <634584067@qq.com>
2020-05-18 14:32:49 +0800

    这是一个初始化的版本

---
+++ video/config/urls.py
@@ -0,0 +1,11 @@
+# conding:utf-8
+
+from django.contrib import admin
+from django.urls import path,include
+from app.client import urls as client_urls
+from app.dashboard import urls as dashboard_urls
+
+urlpatterns = [
+    path('dashboard/',include(dashboard_urls)),
+    path('client/',include(client_urls))
+]
